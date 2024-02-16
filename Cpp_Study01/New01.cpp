8896ee349a5bd70706962d684717d3a59d0e84bd
HA YOUNG <hy01@kakao.com>
2024-02-17 03:10:00 +0900

    20240401study

---
+++ Cpp_Study01/New01.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    printf("2주차 과제를 제출합니다.\n");
+    printf("이름 : 하영\n");
+    printf("학변 : 2024620088\n");
+    printf("학과 : 소프트웨어공학과\n");
+    printf("C 언어를 배우고 있습니다.\n");
+    return 0;
+}
+
+
+
+
+
